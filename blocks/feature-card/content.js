09422dfb6e00a58ba6c24a56ffb3be66cdbd6b55
Salvador Domingues <82028566+salvadord18@users.noreply.github.com>
2025-12-02 17:22:23 +0000

    adjs

--- blocks/feature-card/content.js
+++
@@ -1,9 +0,0 @@
-export const blockContent = {
-  pagetitle: "Correção de endereços",
-  title: "Correção de endereços",
-  body: "Assegura um endereçamento postal normalizado, contribuindo para a eficiência dos processos e redução das devoluções. Melhore a sua base de dados com a identificação e correção de endereços incorretos.",
-  buttonLink: "https://www.ctt.pt",
-  buttonText: "Visitar site",
-  imageSrc: "../assets/images/mail-boxes.png",
-  imageAlt: "Correção de endereços"
-};
